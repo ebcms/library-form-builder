d16d352a575f4a7a1921b83658a496f6356001eb
ebcms <1540837821@qq.com>
2021-01-30 17:01:40 +0800

    1

--- src/Other/Code.php
+++ src/Other/Code.php
@@ -34,11 +34,22 @@ class Code implements ItemInterface
 <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.59.2/mode/javascript/javascript.min.js" integrity="sha512-+tn2IYnLwD2J9p6Nrn/Dl7ag9lluHA0GAblT/vnMiJV8DU/iDsldgf+9XbEqZUee2ThyDtfmSDb+IDZ9u7jrSA==" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.59.2/mode/xml/xml.min.js" integrity="sha512-XPih7uxiYsO+igRn/NA2A56REKF3igCp5t0W1yYhddwHsk70rN1bbbMzYkxrvjQ6uk+W3m+qExHIJlFzE6m5eg==" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.59.2/mode/htmlmixed/htmlmixed.min.js" integrity="sha512-IC+qg9ITjo2CLFOTQcO6fBbvisTeJmiT5D5FnXsCptqY8t7/UxWhOorn2X+GHkoD1FNkyfnMJujt5PcB7qutyA==" crossorigin="anonymous"></script>
+<script>
+    var render_code = function(id){
+        CodeMirror.fromTextArea(document.getElementById(id), {
+            lineNumbers: true,
+            matchBrackets: true,
+            mode: "htmlmixed",
+            indentUnit: 4,
+            indentWithTabs: true,
+        });
+    }
+</script>
 {/if}
 <div class="form-group">
     <label for="field_{:md5($name)}">{$label}</label>
     <textarea
-        class="form-control"
+        class="form-control d-none"
         id="field_{:md5($name)}"
         name="{$name}"
         {if isset($pattern) && $pattern}pattern="{$pattern}"{/if}
@@ -51,15 +62,7 @@ class Code implements ItemInterface
         {if isset($disabled) && $disabled}disabled {/if}
         aria-describedby="help_{:md5($name)}"
     >{$value}</textarea>
-    <script>
-        CodeMirror.fromTextArea(document.getElementById('field_{:md5($name)}'), {
-            lineNumbers: true,
-            matchBrackets: true,
-            mode: "htmlmixed",
-            indentUnit: 4,
-            indentWithTabs: true,
-        });
-    </script>
+    <div class="bg-light p-4 text-secondary" onclick="$(this).remove();render_code('field_{:md5($name)}')"><svg t="1611996487173" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6178" width="18" height="18"><path d="M242.752 268.8l90.496 90.496-152.256 152.256 152.256 152.192-90.496 90.496L0 511.552zM780.992 268.8l-90.56 90.496 152.256 152.256-152.256 152.192 90.56 90.496 242.688-242.688z" fill="#425766" p-id="6179"></path><path d="M513.024 192h128l-128 640h-128z" fill="#9AA8B3" p-id="6180"></path></svg> 点此编辑</div>
     {if isset($help) && $help}
     <small id="help_{:md5($name)}" class="form-text text-muted">{$help}</small>
     {/if}
