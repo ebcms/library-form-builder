ec4b1dd4cbe64b02e784beaa5d44749eb3ad5522
ebcms <56634967+ebcms@users.noreply.github.com>
2021-06-30 16:40:33 +0800

    Update Checkbox.php

--- src/Field/Checkbox.php
+++ src/Field/Checkbox.php
@@ -33,9 +33,9 @@ class Checkbox implements ItemInterface
     <label for="field_{:md5($name)}" class="form-label">{$label}</label>
     <div>
         {foreach $options as $vo}
-        <div class="custom-control custom-checkbox {if $inline}custom-control-inline{/if}">
-            <input class="custom-control-input" type="checkbox" name="{$name}[]" id="field_{:md5($name .'~'. $vo['value'])}" value="{$vo['value']}" {if isset($vo['disabled']) && $vo['disabled']}disabled{else}{:in_array($vo['value'], $value)?'checked':''}{/if}>
-            <label class="custom-control-label" for="field_{:md5($name .'~'. $vo['value'])}">{$vo['label']??$vo['value']}</label>
+        <div class="form-check {if $inline}form-check-inline{/if}">
+            <input class="form-check-input" type="checkbox" name="{$name}[]" id="field_{:md5($name .'~'. $vo['value'])}" value="{$vo['value']}" {if isset($vo['disabled']) && $vo['disabled']}disabled{else}{:in_array($vo['value'], $value)?'checked':''}{/if}>
+            <label class="form-check-label" for="field_{:md5($name .'~'. $vo['value'])}">{$vo['label']??$vo['value']}</label>
         </div>
         {/foreach}
     </div>
