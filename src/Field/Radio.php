09685e448c0aa0b547bc6ddc270f059614298168
ebcms <56634967+ebcms@users.noreply.github.com>
2021-06-30 16:41:14 +0800

    Update Radio.php

--- src/Field/Radio.php
+++ src/Field/Radio.php
@@ -33,9 +33,9 @@ class Radio implements ItemInterface
     <label for="field_{:md5($name)}" class="form-label">{$label}</label>
     <div>
         {foreach $options as $vo}
-        <div class="custom-control custom-radio {if $inline}custom-control-inline{/if}">
-            <input class="custom-control-input" type="radio" name="{$name}" id="field_{:md5($name .'~'. $vo['value'])}" value="{$vo['value']}" {if isset($vo['disabled']) && $vo['disabled']}disabled{/if} {:$vo['value']==$value?' checked':''}>
-            <label class="custom-control-label" for="field_{:md5($name .'~'. $vo['value'])}">{$vo['label']??$vo['value']}</label>
+        <div class="form-check {if $inline}form-check-inline{/if}">
+            <input class="form-check-input" type="radio" name="{$name}" id="field_{:md5($name .'~'. $vo['value'])}" value="{$vo['value']}" {if isset($vo['disabled']) && $vo['disabled']}disabled{/if} {:$vo['value']==$value?' checked':''}>
+            <label class="form-check-label" for="field_{:md5($name .'~'. $vo['value'])}">{$vo['label']??$vo['value']}</label>
         </div>
         {/foreach}
     </div>
