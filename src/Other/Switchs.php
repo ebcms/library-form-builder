cb47987eeb1fd4522a9c9d718a263d0859ca65db
ebcms <56634967+ebcms@users.noreply.github.com>
2021-06-30 16:39:26 +0800

    Update Switchs.php

--- src/Other/Switchs.php
+++ src/Other/Switchs.php
@@ -50,9 +50,9 @@ class Switchs implements ItemInterface
     <label for="field_{:md5($name)}" class="form-label">{$label}</label>
     <div>
         {foreach $switchs as $key=>$vo}
-        <div class="custom-control custom-radio {if $inline}custom-control-inline{/if}">
-            <input class="custom-control-input" type="radio" onclick="$('#{$key}').removeClass('d-none').siblings().addClass('d-none')" name="{$name}" id="field_{:md5($name .'~'. $vo['value'])}" value="{$vo['value']}" {if isset($vo['disabled']) && $vo['disabled']}disabled{/if} {:$vo['value']==$value?' checked':''}>
-            <label class="custom-control-label" for="field_{:md5($name .'~'. $vo['value'])}">{$vo['label']??$vo['value']}</label>
+        <div class="form-check {if $inline}form-check-inline{/if}">
+            <input class="form-check-input" type="radio" onclick="$('#{$key}').removeClass('d-none').siblings().addClass('d-none')" name="{$name}" id="field_{:md5($name .'~'. $vo['value'])}" value="{$vo['value']}" {if isset($vo['disabled']) && $vo['disabled']}disabled{/if} {:$vo['value']==$value?' checked':''}>
+            <label class="form-check-label" for="field_{:md5($name .'~'. $vo['value'])}">{$vo['label']??$vo['value']}</label>
         </div>
         {/foreach}
     </div>
